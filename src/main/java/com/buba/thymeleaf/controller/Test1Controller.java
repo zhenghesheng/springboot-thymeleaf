01e26521c65f0d97879b159f9aeda804156a9adf
zhenghesheng <18337140533@163.com>
2020-12-03 09:33:00 +0800

    Test1Controller.java

---
+++ src/main/java/com/buba/thymeleaf/controller/Test1Controller.java
@@ -0,0 +1,27 @@
+package com.buba.thymeleaf.controller;
+
+/**
+ * @author zhenghs
+ * @see 类名:Test1Controller
+ * @see 描述:测试1
+ * @see 创建日期:  2020/12/3 9:27
+ * @see 修改记录:
+ * @see [编号:日期_设计来源],[修改人:***],[方法名:***]
+ */
+public class Test1Controller {
+    /*
+     * @see 方法名:main
+     * @see 描述:Test1Controller
+     * @see 创建日期:2020/12/3 9:31
+     * @author zhenghs
+     *
+     * @param [args]
+     * @return void
+     *
+     * @see 修改记录:
+     * @see [编号:日期],[修改人:*** ],[修改说明:***]
+     */
+    public static void main(String[] args) {
+        System.out.println("456789");
+    }
+}
